3f71044ce6ea5ba44fe66acd9091c48f87d58440
saya <c3313433633@gmail.com>
2025-07-21 11:53:58 +0800

    update flake.nix

--- template/python/flake.nix
+++ template/python/flake.nix
@@ -8,117 +8,67 @@
 
   };
 
-  outputs =
-    inputs:
-    let
-      system = "x86_64-linux";
-    in
-    {
-      devShells.${system} =
-        let
-          pkgs = import inputs.nixpkgs {
-            config.allowUnfree = true;
-            inherit system;
-            overlays = [
-              (self: super: rec {
-                # https://github.com/NixOS/nixpkgs/blob/c339c066b893e5683830ba870b1ccd3bbea88ece/nixos/modules/programs/nix-ld.nix#L44
-                # > We currently take all libraries from systemd and nix as the default.
-                pythonldlibpath = inputs.nixpkgs.lib.makeLibraryPath (
-                  with super;
-                  [
-                    zlib
-                    zstd
-                    stdenv.cc.cc
-                    curl
-                    openssl
-                    attr
-                    libssh
-                    bzip2
-                    libxml2
-                    acl
-                    libsodium
-                    util-linux
-                    xz
-                    systemd
-                  ]
-                );
-                # here we are overriding python program to add LD_LIBRARY_PATH to it's env
-                python = super.stdenv.mkDerivation {
-                  name = "python";
-                  buildInputs = [ super.makeWrapper ];
-                  src = super.python313;
-                  installPhase = ''
-                    mkdir -p $out/bin
-                    cp -r $src/* $out/
-                    wrapProgram $out/bin/python3 --set LD_LIBRARY_PATH ${pythonldlibpath}
-                    wrapProgram $out/bin/python3.13 --set LD_LIBRARY_PATH ${pythonldlibpath}
-                  '';
-                };
-              })
-            ];
+  outputs = inputs:
+    let system = "x86_64-linux";
+    in {
+      devShells.${system} = let
+        pkgs = import inputs.nixpkgs {
+          config.allowUnfree = true;
+          inherit system;
+          overlays = [
+            (self: super: rec {
+              # https://github.com/NixOS/nixpkgs/blob/c339c066b893e5683830ba870b1ccd3bbea88ece/nixos/modules/programs/nix-ld.nix#L44
+              # > We currently take all libraries from systemd and nix as the default.
+              pythonldlibpath = inputs.nixpkgs.lib.makeLibraryPath
+                (with super; [
+                  zlib
+                  zstd
+                  stdenv.cc.cc
+                  curl
+                  openssl
+                  attr
+                  libssh
+                  bzip2
+                  libxml2
+                  acl
+                  libsodium
+                  util-linux
+                  xz
+                  systemd
+                ]);
+              # here we are overriding python program to add LD_LIBRARY_PATH to it's env
+              python = super.stdenv.mkDerivation {
+                name = "python";
+                buildInputs = [ super.makeWrapper ];
+                src = super.python313;
+                installPhase = ''
+                  mkdir -p $out/bin
+                  cp -r $src/* $out/
+                  wrapProgram $out/bin/python3 --set LD_LIBRARY_PATH ${pythonldlibpath}
+                  wrapProgram $out/bin/python3.13 --set LD_LIBRARY_PATH ${pythonldlibpath}
+                '';
+              };
+            })
+          ];
 
-          };
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              uv
-              python
-              git
-              just
-              ripgrep
-              pre-commit
-            ];
-            shellHook = ''
-              VENV_DIR=".venv"
-              if [ ! -d "$VENV_DIR" ]; then
-                echo ">>> Virtual environment '$VENV_DIR' not found. Creating..."
-                uv venv -p ${pkgs.python313}
-                echo ">>> Virtual environment created."
-              fi
-
-              source "$VENV_DIR/bin/activate"
-
-            '';
-          };
         };
-      packages.${system} =
-        let
-          pkgs = inputs.nixpkgs.legacyPackages.${system};
-        in
-        {
-          default =
-            let
-              python = pkgs.python313;
+      in {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [ uv python git just ripgrep pre-commit ];
+          shellHook = ''
+            VENV_DIR=".venv"
+            if [ ! -d "$VENV_DIR" ]; then
+              echo ">>> Virtual environment '$VENV_DIR' not found. Creating..."
+              uv venv -p ${pkgs.python}
+              echo ">>> Virtual environment created."
+            fi
 
-              project = inputs.pyproject-nix.lib.project.loadUVPyproject {
-                projectRoot = ./src;
-              };
-              attrs = project.renderers.buildPythonPackage {
-                inherit python;
-              };
-              pythonApp = pkgs.python3Packages.buildPythonPackage (attrs);
-              appEnv = pkgs.buildEnv {
-                name = "app-env";
-                paths = [
-                  pythonApp
-                ]; # 在这里可以加入其他依赖, e.g., pkgs.which
-              };
-            in
-            pkgs.dockerTools.buildLayeredImage {
-              # 镜像名称和标签
-              name = "my-python-app";
-              tag = "latest";
-              contents = [
-                appEnv
-              ];
-              config = {
-                Env = [ "USERNAME=nixer" ];
-                Cmd = [
-                  "echo 'hello world!'"
-                ];
-              };
-            };
+            source "$VENV_DIR/bin/activate"
+
+          '';
         };
+      };
+      packages.${system} = let pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in { };
     };
 }
